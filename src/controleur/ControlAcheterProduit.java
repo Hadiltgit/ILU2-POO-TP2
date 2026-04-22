86ba87e773a315deff68041a4b5f95d8200e4f83
Hadiltgit <hadil.takeznount@univ-tlse3.fr>
2026-04-22 22:08:11 +0200

    TP2/3

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -3,19 +3,16 @@ package controleur;
 
 import villagegaulois.Village;
 import villagegaulois.Etal;
+import frontiere.Clavier;
 import personnages.Gaulois;
 
 public class ControlAcheterProduit {
 	private Village village;
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 	private ControlVerifierIdentite controlVerifierIdentite;
-	
-    
-
 
 	public ControlAcheterProduit(ControlVerifierIdentite controlVerifierIdentite,
-			ControlTrouverEtalVendeur controlTrouverEtalVendeur,
-			Village village) {
+			ControlTrouverEtalVendeur controlTrouverEtalVendeur, Village village) {
 		this.village = village;
 		this.controlVerifierIdentite = controlVerifierIdentite;
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
@@ -24,17 +21,35 @@ public class ControlAcheterProduit {
 	public boolean verifierIdentite(String nomAcheteur) {
 		return controlVerifierIdentite.verifierIdentite(nomAcheteur);
 	}
-	
-	
-	public Gaulois[] afficherVendeursProduit(String produit) {
-		return village.rechercherVendeursProduit(produit);
-	}
-	
-	 public int acheterProduit(String nomVendeur, int quantite) {
-	        Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
-	        return etal.acheterProduit(quantite);
-	 }
 
-	
-	
+	public String[] afficherVendeursProduit(String produit) {
+		Gaulois[] vendeurs = village.rechercherVendeursProduit(produit);
+
+		if (vendeurs == null)
+			return null;
+
+		String[] noms = new String[vendeurs.length];
+		for (int i = 0; i < vendeurs.length; i++) {
+			noms[i] = vendeurs[i].getNom();
+		}
+		return noms;
+	}
+
+	public int acheterProduit(String nomVendeur, int quantite) {
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		return etal.acheterProduit(quantite);
+	}
+
+	public String choisirComercant(String produit, String[] vendeurs) {
+
+		System.out.println("Chez quel commerçant voulez-vous acheter des " + produit + " ?\n");
+		for (int i = 0; i < vendeurs.length; i++) {
+			System.out.println((i + 1) + " - " + vendeurs[i] + "\n");
+		}
+		int num = Clavier.entrerEntier("");
+
+		String nomVendeur = vendeurs[num - 1];
+		return nomVendeur;
+	}
+
 }
